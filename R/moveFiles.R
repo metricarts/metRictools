4bf2dcc695288f586be43c2bdb474fda13cbdda6
Ubuntu <dfischer@dfischer.plwwsejojakerjespzeavh1wrh.bx.internal.cloudapp.net>
2019-08-29 20:58:47 +0000

    moveFIles

---
+++ R/moveFiles.R
@@ -0,0 +1,18 @@
+#' Link Files folder
+#'
+#' Move files to Blob
+#' @export
+#' @author Daniel Fischer
+
+moveFiles = function(){
+  safeLibrary("rstudioapi")
+  #createAwsFilesRepo(path = path)
+  
+  
+  current_repo = basename(getwd())
+  param = c(proj = current_repo)
+  dir.create(sqlGsub('"/media/blob/files_project/@proj@/"',param),recursive = T)
+  cmd = sqlGsub('mv "./files/" "/media/blob/files_project/@proj@/"',param)
+  print(cmd)
+  system(cmd)
+}
